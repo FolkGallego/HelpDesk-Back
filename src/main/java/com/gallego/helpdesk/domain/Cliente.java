c0ef610c8c41739feea4bcb34919a0cdbd421257
FolkGallego <joaogallegopolo@gmail.com>
2021-09-07 14:38:34 -0300

    Configurando classes de Dominio, adicionando anotações JPA

--- src/main/java/com/gallego/helpdesk/domain/Cliente.java
+++ src/main/java/com/gallego/helpdesk/domain/Cliente.java
@@ -3,16 +3,26 @@ package com.gallego.helpdesk.domain;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa {
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 
+import com.gallego.helpdesk.domain.enums.Perfil;
+
+@Entity
+public class Cliente extends Pessoa {
+	private static final long serialVersionUID = 1L;
+	
+	@OneToMany(mappedBy = "cliente")
 	private List<Chamado> chamados = new ArrayList<Chamado>();
 
 	public Cliente() {
 		super();
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public Cliente(Integer id, String nome, String cpf, String email, String senha) {
 		super(id, nome, cpf, email, senha);
+		addPerfil(Perfil.CLIENTE);
 	}
 
 	public List<Chamado> getChamados() {
