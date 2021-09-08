cc1a77bea8bea815dff1830ad96b00a34dac816e
FolkGallego <joaogallegopolo@gmail.com>
2021-09-07 21:53:31 -0300

    Adicionando o tratamento de Exceção para objetos não encontrados

--- src/main/java/com/gallego/helpdesk/services/TecnicoService.java
+++ src/main/java/com/gallego/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.gallego.helpdesk.domain.Tecnico;
 import com.gallego.helpdesk.repositories.TecnicoRepository;
+import com.gallego.helpdesk.services.exceptions.ObjectnotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,6 +17,6 @@ public class TecnicoService {
 	
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
 	}
 }
