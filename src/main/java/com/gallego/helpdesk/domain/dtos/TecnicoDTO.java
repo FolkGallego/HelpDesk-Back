06e62eb503d1a2e9b58d3f318a90c4bcfa4d8544
FolkGallego <joaogallegopolo@gmail.com>
2021-09-08 22:51:50 -0300

    Validação nos campos de criação de novo Usuário

--- src/main/java/com/gallego/helpdesk/domain/dtos/TecnicoDTO.java
+++ src/main/java/com/gallego/helpdesk/domain/dtos/TecnicoDTO.java
@@ -6,6 +6,8 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gallego.helpdesk.domain.Tecnico;
 import com.gallego.helpdesk.domain.enums.Perfil;
@@ -14,9 +16,13 @@ public class TecnicoDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	protected Integer id;
+	@NotNull(message = "O campo NOME é requerido")
 	protected String nome;
+	@NotNull(message = "O campo CPF é requerido")
 	protected String cpf;
+	@NotNull(message = "O campo E-MAIL é requerido")
 	protected String email;
+	@NotNull(message = "O campo SENHA é requerido")
 	protected String senha;
 	protected Set<Integer> perfis = new HashSet<>();
 	
