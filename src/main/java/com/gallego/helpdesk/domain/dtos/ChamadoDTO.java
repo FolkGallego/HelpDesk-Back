9c4a03bbe166fe6bee5742f6ce0ad4a45fc5e356
FolkGallego <joaogallegopolo@gmail.com>
2021-09-11 23:29:43 -0300

    Create para chamado

--- src/main/java/com/gallego/helpdesk/domain/dtos/ChamadoDTO.java
+++ src/main/java/com/gallego/helpdesk/domain/dtos/ChamadoDTO.java
@@ -3,6 +3,8 @@ package com.gallego.helpdesk.domain.dtos;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.NotNull;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gallego.helpdesk.domain.Chamado;
 
@@ -14,11 +16,17 @@ public class ChamadoDTO implements Serializable{
 	private LocalDate dateAbertura = LocalDate.now();
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	private LocalDate dataFechamento;
+	@NotNull(message = "O campo PRIORIDADE é requerido ")
 	private Integer prioridade;
+	@NotNull(message = "O campo STATUS é requerido ")
 	private Integer status;
+	@NotNull(message = "O campo TITULO é requerido ")
 	private String titulo;
+	@NotNull(message = "O campo OBSERVAÇÕES é requerido ")
 	private String observacoes;
+	@NotNull(message = "O campo TECNICO é requerido ")
 	private Integer tecnico;
+	@NotNull(message = "O campo CLIENTE é requerido ")
 	private Integer cliente;
 	private String nomeTecnico;
 	private String nomeCliente;
--- src/main/java/com/gallego/helpdesk/services/ChamadoService.java
+++ src/main/java/com/gallego/helpdesk/services/ChamadoService.java
@@ -3,10 +3,17 @@ package com.gallego.helpdesk.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.gallego.helpdesk.domain.Chamado;
+import com.gallego.helpdesk.domain.Cliente;
+import com.gallego.helpdesk.domain.Tecnico;
+import com.gallego.helpdesk.domain.dtos.ChamadoDTO;
+import com.gallego.helpdesk.domain.enums.Prioridade;
+import com.gallego.helpdesk.domain.enums.Status;
 import com.gallego.helpdesk.repositories.ChamadoRepository;
 import com.gallego.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -15,7 +22,11 @@ public class ChamadoService {
 
 	@Autowired
 	private ChamadoRepository repository;
-	
+	@Autowired
+	private TecnicoService tecnicoService;
+	@Autowired
+	private ClienteService clienteService;
+
 	public Chamado findById(Integer id) {
 		Optional<Chamado> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
@@ -24,6 +35,27 @@ public class ChamadoService {
 	public List<Chamado> findAll() {
 		return repository.findAll();
 	}
-	
-	
+
+	public Chamado create(@Valid ChamadoDTO objDTO) {
+		return repository.save(newChamado(objDTO));
+	}
+
+	private Chamado newChamado(ChamadoDTO obj) {
+		Tecnico tecnico = tecnicoService.findById(obj.getTecnico());
+		Cliente cliente = clienteService.findById(obj.getCliente());
+
+		Chamado chamado = new Chamado();
+		if (obj.getId() != null) {
+			chamado.setId(obj.getId());
+		}
+
+		chamado.setTecnico(tecnico);
+		chamado.setCliente(cliente);
+		chamado.setPrioridade(Prioridade.toEnum(obj.getPrioridade()));
+		chamado.setStatus(Status.toEnum(obj.getStatus()));
+		chamado.setTitulo(obj.getTitulo());
+		chamado.setObservacoes(obj.getObservacoes());
+		return chamado;
+
+	}
 }
